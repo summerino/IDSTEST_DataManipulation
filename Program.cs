26f408b4c6f61619e326429630a23972410bef57
Galang Bilal <galang.bilal@sahassa.co.id>
2024-08-08 17:17:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] valArr = [1,2,2,3];
+        int totalPair = 4;
+        Console.WriteLine(CountPair(valArr, totalPair));
+    }
+
+    private static int CountPair(int[] valArr, int totalPair)
+    {
+        Dictionary<int, int > pairs = [];
+        int count = 0;
+        foreach (int i in valArr)
+        {
+            int pair = totalPair - i;
+
+            if (pairs.TryGetValue(pair, out int value))
+            {
+                count += value;
+            }
+            pairs[i] = pairs.GetValueOrDefault(i, 0) + 1;
+        }
+
+        return count;
+    }
+}
